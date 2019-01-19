ba6f8ab8bec28ec7ca12a3a795d8d70a54fcbf40
wizard24jr <46851264+wizard24jr@users.noreply.github.com>
2019-01-19 15:58:49 -0600

    Delete title.js

--- assets/js/title.js
+++
@@ -1,4 +0,0 @@
-if(document.addEventListener){document.addEventListener("DOMContentLoaded",function(){loaded();});}else if(document.attachEvent){document.attachEvent("onreadystatechange",function(){loaded();});}
-function loaded(){setInterval(loop,400);}
-var x=0;var titleText=["d","da","dam","dama","damag","damage","damaged","damaged.","damaged.c","damaged.cl","damaged.clu","damaged.club"];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
-window.oncontextmenu=function(){console.log("Where do we come from?");return false;}
